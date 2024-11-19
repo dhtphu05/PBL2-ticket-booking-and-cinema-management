181478896e996f4328bb1e8521ce9f7e918a35f3
Doan Hoang Thien Phu <dhtphu05@gmail.com>
2024-11-20 00:57:51 +0700

    complete func sell ticket

--- Libraries/Screen.cpp
+++ Libraries/Screen.cpp
@@ -20,8 +20,6 @@ void Screen::loadScreenFromFile(DoubleLinkedList<Screen> &screens) {
         cout << "Error opening Screen.txt for reading" << endl;
         return;
     }
-
-
     string line, key;
     Screen* currentScreen = nullptr;
 
@@ -103,19 +101,11 @@ void Screen::loadScreenFromFile(DoubleLinkedList<Screen> &screens) {
             }
         }
     }
-
-
     // Thêm màn hình cuối cùng vào danh sách nếu có
     if (currentScreen != nullptr) {
         screens.push_back(*currentScreen);
          // Free the previous screen's memory
     }
-
-    if (currentScreen != nullptr) {
-        screens.push_back(*currentScreen);
-         // Free the previous screen's memory
-    }
-
     file.close();
 }
 
@@ -126,6 +116,7 @@ void Screen::displayScreen(){
     for(Node<Seat>* node = this->seatLayout.begin(); node != nullptr; node = node->next){
         Seat seat = node->data;
         seat.displaySeat();
+        cout<<endl;
     }
 }
 void Screen::displayAllScreen(DoubleLinkedList<Screen> &screens){
@@ -190,7 +181,6 @@ bool Screen::saveScreenToFile(DoubleLinkedList<Screen> screens){
        
         file << endl;
          file<<"# NextScreen"<<endl;
-         file<<"# NextScreen"<<endl;
         current = current->next;
     }
     file.close();
@@ -235,18 +225,10 @@ void Screen::displayScreenSimpleLayout(){
    
     cout<<endl;
 }
-void Screen::displayAllScreenSimpleLayout(DoubleLinkedList<Screen> &screens){
+Screen* Screen::selectScreen(DoubleLinkedList<Screen>&screens,int ID_screen){
     for(Node<Screen>* node = screens.begin(); node != nullptr; node = node->next){
-        node->data.displayScreenSimpleLayout();
-        cout<<"# NextScreen"<<endl;
-    }
-}
-Screen* Screen::selectScreen(int ID_screen){
-    DoubleLinkedList<Screen> screens;
-    Screen screen;
-    screen.loadScreenFromFile(screens);
-    for(Node<Screen>* node = screens.begin(); node != nullptr; node = node->next){
-        if(node->data.ID_Screen==ID_screen){
+        if(node->data.getID_screen()==ID_screen){
+            cout<<"huhu"<<node->data.getID_screen()<<"huhu"<<endl;
             return &node->data;
         }
     }
@@ -257,45 +239,14 @@ Screen* Screen::selectScreen(int ID_screen){
 int Screen::getID_screen(){
     return ID_Screen;
 }
-void Screen::displayScreenSimpleLayout(){
-    cout<<"Screen ID: "<<this->ID_Screen<<endl;
-    cout<<"Screen Layout: "<<endl;
-    int rowIdx = 0;
-    for(Node<Seat>* node = this->seatLayout.begin(); node != nullptr; node = node->next){
-        Seat seat = node->data;
-        if(seat.getType()==SeatType::VIP){
-                cout << "V ";
-            } else if(seat.getType()==SeatType::Regular){
-                cout << "R ";
-            } else if(seat.getType()==SeatType::Disable){
-                cout << "D ";
-            }
-            cout<<" ";
-            if(seat.getSeatColumn()==9){
-                cout << endl;
-            }
-        
-    }
-    
-    cout<<endl;
-}
+
 void Screen::displayAllScreenSimpleLayout(DoubleLinkedList<Screen> &screens){
     for(Node<Screen>* node = screens.begin(); node != nullptr; node = node->next){
         node->data.displayScreenSimpleLayout();
         cout<<"# NextScreen"<<endl;
     }
 }
-Screen* Screen::selectScreen(int ID_screen){
-    DoubleLinkedList<Screen> screens;
-    Screen screen;
-    screen.loadScreenFromFile(screens);
-    for(int i=0;i<screens.getSize();i++){
-        if(screens[i].ID_Screen==ID_screen){
-            return new Screen(screens[i]);
-        }
-    }
-    return nullptr;
-}
+
 void printScreen(Screen* screen){
     cout<<screen->getID_screen()<<endl;
 }
